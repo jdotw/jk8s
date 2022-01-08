8dfc9a5cf99bd94c5895c06a124d931e626a151e
James Wilson <jw@jameswilson.io>
2022-01-09 10:27:14 +1100

    removed unused stacks

--- provision/aws-eks/cdk/bin/cdk.ts
+++ provision/aws-eks/cdk/bin/cdk.ts
@@ -3,11 +3,9 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import { RDSStack } from "../lib/rds-stack";
-import { TelemetryStack } from "../lib/telemetry-stack";
 import { EKSStack } from "../lib/eks-stack";
 import { VPCStack } from "../lib/vpc-stack";
 import { DNSStack } from "../lib/dns-stack";
-import { CertStack } from "../lib/cert-stack";
 import { SecretsStack } from "../lib/secrets-stack";
 import { OpenSearchStack } from "../lib/opensearch-stack";
 
@@ -40,17 +38,3 @@ const cluster = new EKSStack(app, "EKSStack", {
 });
 
 const opensearch = new OpenSearchStack(app, "OpenSearchStack", { vpc });
-
-const cert = new CertStack(app, "CertStack", {
-  name,
-  dns,
-});
-
-// const bs = new BootstrapStack(app, "BootstrapStack", {
-//   name,
-//   vpc,
-//   rds,
-//   dns,
-//   cluster,
-//   cert,
-// });
