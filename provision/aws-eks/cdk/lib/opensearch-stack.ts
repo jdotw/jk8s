85907c841781b2fbf5aef8b5aaeb631ed845a279
James Wilson <jw@jameswilson.io>
2022-01-11 11:31:26 +1100

    jaeger es credentials secret

--- provision/aws-eks/cdk/lib/opensearch-stack.ts
+++ provision/aws-eks/cdk/lib/opensearch-stack.ts
@@ -2,11 +2,13 @@ import { Stack, StackProps } from "aws-cdk-lib";
 import * as cdk from "aws-cdk-lib";
 import * as rds from "aws-cdk-lib/aws-rds";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as secretsmanager from "aws-cdk-lib/aws-secretsmanager";
 import * as opensearch from "aws-cdk-lib/aws-opensearchservice";
 import { Vpc } from "aws-cdk-lib/aws-ec2";
 import { Construct } from "constructs";
 import { DatabaseInstance } from "aws-cdk-lib/aws-rds";
 import { VPCStack } from "./vpc-stack";
+import { domain } from "process";
 
 export interface OpenSearchStackProps extends StackProps {
   vpc: VPCStack;
@@ -14,6 +16,17 @@ export interface OpenSearchStackProps extends StackProps {
 export class OpenSearchStack extends Stack {
   constructor(scope: Construct, id: string, props?: OpenSearchStackProps) {
     super(scope, id, props);
+
+    const masterUserSecret = new secretsmanager.Secret(
+      this,
+      "MasterUserSecret",
+      {
+        generateSecretString: {
+          secretStringTemplate: JSON.stringify({ username: "master-user" }),
+          generateStringKey: "password",
+        },
+      }
+    );
 
     const prodDomain = new opensearch.Domain(this, "Domain", {
       version: opensearch.EngineVersion.OPENSEARCH_1_0,
@@ -34,12 +47,27 @@ export class OpenSearchStack extends Stack {
         appLogEnabled: true,
         slowIndexLogEnabled: true,
       },
+      enforceHttps: true,
+      nodeToNodeEncryption: true,
+      encryptionAtRest: {
+        enabled: true,
+      },
+      fineGrainedAccessControl: {
+        masterUserName: "master-user",
+        masterUserPassword: masterUserSecret.secretValue,
+      },
     });
 
     new cdk.CfnOutput(this, "OpenSearchDomain", {
       value: prodDomain.domainEndpoint,
       description: "OpenSearch Domain",
       exportName: "OpenSearchDomain",
+    });
+
+    new cdk.CfnOutput(this, "MasterUserSecretName", {
+      value: masterUserSecret.secretName,
+      description: "OpenSearch Master User Secret Name",
+      exportName: "MasterUserSecretName",
     });
   }
 
